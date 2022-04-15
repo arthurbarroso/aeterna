fd0940d88684ff7142354dfa99a98e5ea24db6c9
arthurbarroso <arthurdsb_@hotmail.com>
2022-04-14 21:03:26 -0300

    chore: cleanup

--- src/lamb/background/core.cljs
+++ src/lamb/background/core.cljs
@@ -1,11 +1,7 @@
 (ns lamb.background.core
-  (:require [goog.string :as gstring]
-            [goog.string.format]
-            [cljs.core.async :refer [go-loop <! >! chan go]]
-            [chromex.logging :refer-macros [log]]
+  (:require [cljs.core.async :refer [go-loop <! >! chan go]]
             [chromex.chrome-event-channel :as ec]
             [chromex.ext.tabs :as tabs]
-            [chromex.ext.runtime :as runtime]
             [lamb.background.storage :as storage]
             [lamb.background.selection :as selection]
             [chromex.ext.context-menus :as cm]))
@@ -43,32 +39,24 @@
       (save-selection!!)
       nil)))
 
-(defn process-chrome-event [event-num event]
-  (log (gstring/format "BACKGROUND: got chrome event (%05d)" event-num)
-       event)
-  (log (first event))
+(defn process-chrome-event [event]
   (let [[event-id event-args] event]
     (case event-id
       ::cm/on-clicked (handle-context-click event-args)
       nil)))
 
 (defn run-chrome-event-loop! [chrome-event-channel]
-  (log "BACKGROUND: starting main event loop...")
-  (go-loop [event-num 1]
+  (go-loop []
     (when-some [event (<! chrome-event-channel)]
-      (process-chrome-event event-num event)
-      (recur (inc event-num)))
-    (log "BACKGROUND: leaving main event loop")))
+      (process-chrome-event event)
+      (recur))))
 
 (defn boot-chrome-event-loop! []
   (let [chrome-event-channel (ec/make-chrome-event-channel (chan))]
-    (tabs/tap-all-events chrome-event-channel)
     (cm/tap-on-clicked-events chrome-event-channel)
-    (runtime/tap-all-events chrome-event-channel)
     (run-chrome-event-loop! chrome-event-channel)))
 
 (defn init! []
-  (log "BACKGROUND: init")
   (cm/create (clj->js {:title "my-ext-dev"
                        :id "my-exit-ev"
                        :contexts ["all"]}))
