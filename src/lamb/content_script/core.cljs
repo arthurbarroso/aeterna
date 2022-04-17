d9f578591418f547d8dcecc4c5879be1c8c6f2fb
arthurbarroso <arthurdsb_@hotmail.com>
2022-04-17 12:59:11 -0300

    chore: rm content_script stuff once again

--- src/lamb/content_script/core.cljs
+++ src/lamb/content_script/core.cljs
@@ -1,25 +1,3 @@
-(ns lamb.content-script.core
-  (:require [cljs.core.async :refer [go-loop <!]]
-            [chromex.protocols.chrome-port :refer [post-message!]]
-            [chromex.ext.runtime :as runtime]))
-
-(defn process-message! [message]
-  (.log js/console "CONTENT SCRIPT: got message:" message))
-
-(defn run-message-loop! [message-channel]
-  (.log js/console "CONTENT SCRIPT: starting message loop...!")
-  (go-loop []
-    (.log js/console "entered go loop")
-    (when-some [message (<! message-channel)]
-      (.log js/console "run go loop")
-      (process-message! message)
-      (recur))
-    (.log js/console "CONTENT SCRIPT: ending go loop...")))
-
-(defn connect-to-background-page! []
-  (let [background-port (runtime/connect)]
-    (post-message! background-port "hello from CONTENT SCRIPT!")
-    (run-message-loop! background-port)
-    (post-message! background-port "hello from CONTENT SCRIPT!")))
+(ns lamb.content-script.core)
 
 (defn init! [])
